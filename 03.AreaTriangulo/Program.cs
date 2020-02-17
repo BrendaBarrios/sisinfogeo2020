4b5529d15461524ad4e911d0880f1b967d57592a
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2020-02-17 17:30:18 -0600

    Calculando area del triangulo

---
+++ 03.AreaTriangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03.AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double labase,elaltura,elarea;
+            Console.WriteLine("Calcula area del triangulo");
+            Console.WriteLine("Dame la base:");
+            labase=double.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la altura:");
+            elaltura=double.Parse(Console.ReadLine());
+
+            elarea=(labase*elaltura)/2;
+            Console.WriteLine($"El area del triangulo{elarea}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
