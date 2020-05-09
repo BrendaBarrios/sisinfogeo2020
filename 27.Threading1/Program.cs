27ac82069429d43fabcd988c93ad5be77c6abf0d
BrendaBarrios <51673821+BrendaBarrios@users.noreply.github.com>
2020-05-08 19:07:09 -0500

    Utilizando Treading ejemplo simple
    Primer ejercicio utilizando threading.

---
+++ 27.Threading1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace _27.Threading1
+{
+    class program{
+        static void Main(string[] args){
+
+            //Nombre del thread principal.
+            Thread.CurrentThread.Name="Principal";
+
+            //Creamos dos threads
+            Thread t1 = new Thread(Imprime);
+            Thread t2 = new Thread(Imprime);
+
+            //Damos un nombre a los thread que creamos
+            t1.Name="Thread1";
+            t2.Name="Thread2";
+
+            // Invocams los threads
+            t1.Start();
+            t2.Start();
+        
+            //Invocamosimprime desde el thread principal.
+            Imprime();
+
+            Console.WriteLine("Saludos desde MAIN , se ha terminado todo");
+
+        }
+
+        static void Imprime(){
+            for (int i=0; i<10; i++){
+                Console.WriteLine($"Imprime.{i} desde {Thread.CurrentThread.Name}");
+                Thread.Sleep(100);
+            }
+        }
+    }
+    
+}
