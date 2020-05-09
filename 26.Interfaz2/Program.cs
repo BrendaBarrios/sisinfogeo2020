0877be1191dbe47c5c1174e3f5f8aaffd71989e6
BrendaBarrios <51673821+BrendaBarrios@users.noreply.github.com>
2020-05-08 19:04:56 -0500

    Ejercicios de interfaces 2
    Se realiza otro ejemplo de interfaces.

---
+++ 26.Interfaz2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace _26.Interfaz2
+{
+    
+    public class Organismo{
+
+        public void Respiracion(){
+            Console.WriteLine("Respiración");
+        }
+
+        public void Movimiento(){
+            Console.WriteLine("Movimiento");
+        }
+            
+        public void Crecimiento(){
+            Console.WriteLine("Crecimiento");
+        }
+
+    }
+
+    public interface IAnimales{
+        void Multicelular();
+        void SangreCaliente();
+
+    }
+
+    public interface ICanino : IAnimales{
+        void Corre();
+        void CuatroPatas();
+    }
+
+    public interface IPajaro : IAnimales{
+        void Vuela();
+        void DosPatas();
+    }
+
+    public class Perro : Organismo , ICanino {
+        public void Multicelular() => Console.WriteLine("Multicelular");
+        public void SangreCaliente() => Console.WriteLine("Sangre caliente");
+        public void Corre() => Console.WriteLine("Corre");
+        public void CuatroPatas() => Console.WriteLine("Cuatro Patas");
+
+    }
+
+    public class Perico : Organismo , IPajaro {
+        public void Multicelular() => Console.WriteLine("Multicelular");
+        public void SangreCaliente() => Console.WriteLine("Sangre caliente");
+        public void Vuela() => Console.WriteLine("Vuela");
+        public void DosPatas() => Console.WriteLine("Dos Patas");
+
+    }
+
+    class Program{
+        static void Main(string[] args){
+
+            Perro perro = new Perro();
+            Console.WriteLine("\n Caracteristicas del perro");
+            perro.Respiracion();
+            perro.Movimiento();
+            perro.Crecimiento();
+            perro.Corre();
+            perro.CuatroPatas();
+
+            Perico pajaro = new Perico();
+            Console.WriteLine("\n Caracteristicas del perro");
+            pajaro.Respiracion();
+            pajaro.Movimiento();
+            pajaro.Crecimiento();
+            pajaro.Vuela();
+            pajaro.DosPatas();
+        }
+    }
+
+}
