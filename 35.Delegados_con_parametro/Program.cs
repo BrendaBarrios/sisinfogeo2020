20bd2695d9c98b5df19ce782d1eb0fd021742a5d
BrendaBarrios <51673821+BrendaBarrios@users.noreply.github.com>
2020-05-16 22:10:05 -0500

    Ejemplo de delagado con parametros
    Ejemplo de delegados con parametros

---
+++ 35.Delegados_con_parametro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _35.Delegados_con_parametro
+{
+    public delegate void  MiDelegado(string msj);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MiDelegado del; // Se crea instancia de MiDelegado
+
+            Console.Clear();
+
+            del=ClaseA.MetodoA;
+            InvocaDelegado(del);
+
+            del=ClaseB.MetodoB;
+            InvocaDelegado(del);
+
+            del = (string msj) => Console.WriteLine($"Llamando expresión Lambada : {msj}");
+            InvocaDelegado(del);
+
+            Console.WriteLine();
+            }
+
+            static void InvocaDelegado(MiDelegado del){
+                del("Hola Mundo");
+            }
+        }
+
+    class ClaseA{
+        public static void MetodoA(string msj){
+            Console.WriteLine($"Llamando MetodoA de ClaseA : {msj}");
+        }
+    }
+
+    class ClaseB{
+        public static void MetodoB(string msj){
+            Console.WriteLine($"Llamando MetodoA de ClaseAB: {msj}");
+        }
+    }
+}
+
+
