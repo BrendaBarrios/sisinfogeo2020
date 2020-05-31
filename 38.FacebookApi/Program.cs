25ea4a2292062bc404149e8ffa7cea19824f323c
BrendaBarrios <51673821+BrendaBarrios@users.noreply.github.com>
2020-05-31 16:12:14 -0500

    Ejemplo de conexión con la  API de facebook
    Realización de un ejemplo con la API de facebook

---
+++ 38.FacebookApi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace _38.FacebookApi
+{
+    //Ejemplo de acceso a la API de facebook.
+    class Program
+    {
+        public static object JsonConvert { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------- Programa 38 Acceso a la API de facebook ----------");
+
+            //token.
+            string fbtoken= "EAAS5VGo3KKMBACS3QsYeFkZCFBDahppuMw4TG3aiLmHC4zd0fzhYNP7FIpXOln2BVH4YUMBCKppzaaCbLk78NeCeghvx9gtuG3mhcannDl8Sj7EmFv0ZAxiE4ZBsxZCVddsOEb14hcPtMXMvlufVcXxgryho5BZAzepsxG8O4S0Kt9TsjBcaejyfMgqiXOYOeigTnCoT682X4ZBZA05hQxjpZAHkPDgzbf1V76gRpQ7OPQZDZD";
+            string fbfields= "id,first_name,last_name,middle_name,name,name_format,picture,short_name,email";
+
+            FacebookApi fb=new FacebookApi(fbtoken);
+
+            string resultado= fb.ObtenerInfo(fbfields).Result;
+
+            var res = JsonConvert.DeserializeObject(resultado);
+        
+
+            Console.WriteLine(res);
+
+        }
+    }
+
+
+}
